980f6ff8af0595bfe411f3b0f486235748306a1e
wachira <wachiraa954@gmail.com>
2022-06-03 13:49:21 +0300

    commit tree 41088ddd37ea57fd55351bedfa53668ca33cbb06

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string;
+    //todo
+}
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string;
+    //todo
+
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+ 
+function sayHiToHeadphonedRoommate(string) {
+    if(string.toLowerCase()===string){
+        return "I can't hear you"
+    }
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toUpperCase()=== string){
+        return "YES INDEED!"
+    }
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "Let's have dinner together!"){
+        return "I would love to!"
+        
+    }
+}
